04d77ec7e856cb9fdd40d989860ef6f34bbb7e85
DoktorMandarin <faa1995alex@gmail.com>
2022-06-16 19:34:31 +0300

    Добавьте файлы проекта.

---
+++ 2.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+
+
+namespace _2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            var number = rand.Next(0, 100);
+            var moduleMultiplicity1 = 3;
+            var moduleMultiplicity2 = 5;
+            var summ = number;
+
+            Console.WriteLine(number);
+
+            for (var i = 0; i < number; i++)
+            {
+                if (i % moduleMultiplicity1 == 0 || i % moduleMultiplicity2 == 0)
+                {
+                    summ += i;
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine("\n" + summ);
+        }
+    }
+}
